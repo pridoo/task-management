f0fe7dbd00e0b5ff8f89ebde624e61efd195fe82
faye <fayeandrieaabangan@gmail.com>
2025-05-08 15:15:35 +0800

    pang ilang commit ko na eto baks

--- routes/web.php
+++ routes/web.php
@@ -51,20 +51,15 @@ Route::prefix('admin')->name('admin.')->middleware(['auth:admin', 'admin'])->gro
         Route::get('/to-do', [TaskController::class, 'todo'])->name('tasks.to-do');
         Route::get('/in-progress', [TaskController::class, 'inprogress'])->name('tasks.in-progress');
         Route::get('/completed', [TaskController::class, 'completed'])->name('tasks.completed');
-<<<<<<< HEAD
+
         Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
-<<<<<<< HEAD
-=======
-=======
+
         Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('user.tasks.show');
->>>>>>> origin/alfred
         Route::post('/tasks/{task}/comments', [TaskController::class, 'storeComment'])->name('comments.store');
         Route::put('/notifications/mark-as-read/{id}', [TaskController::class, 'markAsRead'])->name('notifications.markAsRead');
         Route::get('/activity/{id}/read', [TaskController::class, 'markActivityAsRead'])->name('tasks.markActivityAsRead');
 
         
-
->>>>>>> origin/alfred
     });
 
    
