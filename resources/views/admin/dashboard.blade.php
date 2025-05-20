79c36d3f888d9a3791a4faf28f2c5cd8b5e241dd
alfred <cabatoalfred725@gmail.com>
2025-05-20 13:25:09 +0800

    fixing bugs

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
 
 <body class="text-gray-800 font-inter">
     <main class="w-full md:w-[calc(100%-256px)] md:ml-64 bg-gray-50 min-h-screen transition-all main overflow-y-auto">
-        <div class="py-2 px-6 bg-white flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30">
+        <div class="py-2 px-6 bg-white flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30"> 
             <ul class="flex items-center text-sm ml-4">
                 <li class="mr-2">
                     <a href="#" class="text-gray-400 hover:text-gray-600 font-medium">Admin Dashboard</a>
